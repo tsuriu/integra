0c6cd30a1909f90cebeef08f18ef39c28e8cae56
Tulio Amancio <tsuriu@users.noreply.github.com>
2022-08-22 00:20:21 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,32 @@
 module.exports = {
   apps : [{
-    name: 'integraveloo',
+    name: 'integra',
     cmd: 'src/app.py',
     interpreter: '.env/bin/python3',   
     autorestart: true,
     watch: true,
-    pid: '/tmp/sqdintegra.pid',
+    pid: '/tmp/integra.pid',
     instances: 1,
     max_memory_restart: '1G',
     env: {
       ENV: 'development',
       PYTHONWARNINGS: 'ignore:Unverified HTTPS request',
-      IXC_DOMAIN: 'ixc.veloo.com.br',
-      IXC_KEY: '165:8e1817e023f5905385b7035208a67c3be5b5a6b08b97dae0dabe33da7bcb21b2', //tulio user token
-      RC_URL: 'https://chat.veloo.com.br/',
+      IXC_DOMAIN: 'ixc.test.com.br',
+      IXC_KEY: '', //tulio user token
+      RC_URL: 'https://chat.test.com.br/',
       RC_USERNAME: 'vbot',
-      RC_PASSWORD: 'v3l00@v3l00',
+      RC_PASSWORD: 'RC_BOT_PASS',
       PORT: '3000',
       DEBUG: 'True'
     },
     env_production : {
       ENV: 'production',
       PYTHONWARNINGS: 'ignore:Unverified HTTPS request',
-      IXC_DOMAIN: 'ixc.veloo.com.br',
-      IXC_KEY: '178:a4bfdccc42d6a6af79b9ad7b7007ee91b03fb2e8b7433629a89e77a036da2a60', //veloobot user token
-      RC_URL: 'https://chat.veloo.com.br/',
+      IXC_DOMAIN: 'ixc.test.com.br',
+      IXC_KEY: '', //bot user token
+      RC_URL: 'https://chat.test.com.br/',
       RC_USERNAME: 'vbot',
-      RC_PASSWORD: 'v3l00@v3l00',
+      RC_PASSWORD: 'RC_BOT_PASS',
       PORT: '8080',
       DEBUG: 'False'
     }
